882ce381b547573058c31ca2b4bb439b09c2ac68
Christopher Farrow <farrowch@gmail.com>
2010-11-02 20:34:39 +0000

    Added a hard test. Things seem to work.

--- tests/testrefine.py
+++ tests/testrefine.py
@@ -11,11 +11,14 @@ import numpy
 # useful variables
 thisfile = locals().get('__file__', 'file.py')
 tests_dir = os.path.dirname(os.path.abspath(thisfile))
-# testdata_dir = os.path.join(tests_dir, 'testdata')
+testdata_dir = os.path.join(tests_dir, 'testdata')
 
 from diffpy.pdfmorph.morphs.morphchain import MorphChain
 from diffpy.pdfmorph.morphs.morphscale import MorphScale
 from diffpy.pdfmorph.morphs.morphstretch import MorphStretch
+from diffpy.pdfmorph.morphs.morphsmear import MorphSmear
+from diffpy.pdfmorph.morphs.morphpdftordf import MorphXtalPDFtoRDF
+from diffpy.pdfmorph.morphs.morphrdftopdf import MorphXtalRDFtoPDF
 from diffpy.pdfmorph.refine import refine, refinefmin
 
 class TestRefine(unittest.TestCase):
@@ -54,8 +57,8 @@ class TestRefine(unittest.TestCase):
 
         # Define the morphs
         config = {
-                "scale" : 3.0,
-                "stretch" : 0.4
+                "scale" : 1.0,
+                "stretch" : 0.0
                 }
 
         mscale = MorphScale(config)
@@ -76,6 +79,53 @@ class TestRefine(unittest.TestCase):
         return
 
 # End of class TestRefine
+
+class TestRefineUC(unittest.TestCase):
+
+    def setUp(self):
+        objfile = os.path.join(testdata_dir, "nickel_ss0.01.cgr")
+        self.xobj, self.yobj = numpy.loadtxt(objfile, unpack = True, skiprows
+                = 8)
+        reffile = os.path.join(testdata_dir, "nickel_ss0.02_eps0.002.cgr")
+        self.xref, self.yref = numpy.loadtxt(reffile, unpack = True, skiprows
+                = 8)
+        return
+
+    def test_refine(self):
+        config = {
+                "scale" : 1.0,
+                "stretch" : 0,
+                "smear" : 0,
+                }
+
+        from diffpy.pdfmorph.tools import estimateBaselineSlope
+        slope = estimateBaselineSlope(self.xobj, self.yobj)
+        config["baselineslope"] = slope
+
+        mpdftordf = MorphXtalPDFtoRDF(config)
+        mscale = MorphScale(config)
+        mstretch = MorphStretch(config)
+        msmear = MorphSmear(config)
+        mrdftopdf = MorphXtalRDFtoPDF(config)
+        chain = MorphChain(config, mpdftordf, mscale, mstretch, msmear,
+                mrdftopdf)
+
+        # Do this as two-stage fit
+        res = refine(chain, self.xobj, self.yobj, self.xref, self.yref,
+                "scale", "smear")
+        res = refine(chain, self.xobj, self.yobj, self.xref, self.yref,
+                "scale", "stretch", "smear")
+
+        xobj, yobj, xref, yref = chain.xyallout
+
+        # We want the fit good to 1%. We will disregard the last bit of the
+        # fit, since we know we have unavoidable edge effects there.
+        sel = xobj < 9.5
+        yrsel = yref[sel]
+        diff = yrsel - yobj[sel]
+        rw = (numpy.dot(diff, diff) / numpy.dot(yrsel, yrsel))**0.5
+        self.assertTrue(rw < 0.01)
+        return
 
 if __name__ == '__main__':
     unittest.main()
