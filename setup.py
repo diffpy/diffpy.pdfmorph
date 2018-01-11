ff6eeb1f65d71108f2b7075df0798dab00fd6a2c
christopher <cjwright4242@gmail.com>
2018-01-11 13:25:57 -0500

    begin pep8 and push for travis

--- setup.py
+++ setup.py
@@ -11,30 +11,30 @@ from setuptools import setup, find_packages
 
 # define distribution
 setup(
-        name = "diffpy.pdfmorph",
-        version = "1.0",
-        namespace_packages = ['diffpy'],
-        packages = find_packages(exclude=['tests', 'applications']),
-        entry_points = {
-            # define console_scripts here, see setuptools docs for details.
-            'console_scripts' : [
-                'pdfmorph = diffpy.pdfmorph.pdfmorphapp:main',
-            ],
-        },
-        test_suite = 'tests',
-        install_requires = [],
-        dependency_links = [
-            'http://www.diffpy.org/packages/',
-            ],
-        author = 'Simon J.L. Billinge',
-        author_email = 'sb2896@columbia.edu',
-        maintainer = 'Chris Farrow',
-        maintainer_email = 'clf2121@columbia.edu',
-        url = 'http://www.diffpy.org/',
-        download_url = 'http://www.diffpy.org/packages/',
-        description = "Tools for manipulating and comparing PDF profiles.",
-        license = 'BSD',
-        keywords = "diffpy PDF",
+    name="diffpy.pdfmorph",
+    version="1.0",
+    namespace_packages=['diffpy'],
+    packages=find_packages(exclude=['tests', 'applications']),
+    entry_points={
+        # define console_scripts here, see setuptools docs for details.
+        'console_scripts': [
+            'pdfmorph = diffpy.pdfmorph.pdfmorphapp:main',
+        ],
+    },
+    test_suite='tests',
+    install_requires=[],
+    dependency_links=[
+        'http://www.diffpy.org/packages/',
+    ],
+    author='Simon J.L. Billinge',
+    author_email='sb2896@columbia.edu',
+    maintainer='Chris Farrow',
+    maintainer_email='clf2121@columbia.edu',
+    url='http://www.diffpy.org/',
+    download_url='http://www.diffpy.org/packages/',
+    description="Tools for manipulating and comparing PDF profiles.",
+    license='BSD',
+    keywords="diffpy PDF",
 )
 
 # End of file
