9308188c893662b0e2c3cff393af48b8b8a570f8
christopher <cjwright4242@gmail.com>
2018-01-11 14:06:13 -0500

    remove unused test function

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -15,9 +15,6 @@ thisfile = locals().get('__file__', 'file.py')
 tests_dir = os.path.dirname(os.path.abspath(thisfile))
 testdata_dir = os.path.join(tests_dir, 'testdata')
 
-def testdata(filename):
-    return os.path.join(testdata_dir, filename)
-
 import diffpy.pdfmorph.tools as tools
 
 ##############################################################################
