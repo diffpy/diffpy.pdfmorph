b33e620c227f5416535eca5849ca9d8147ba49b9
Timothy Liu <chbrahmsliu@gmail.com>
2018-02-06 17:51:54 -0500

    DEV: finish function call up to scale, stretch and smear

--- diffpy/pdfmorph/morph_tool.py
+++ diffpy/pdfmorph/morph_tool.py
@@ -1,13 +1,28 @@
-def morph_engine(xobj, yobj, xref, yref, morph_config):
+from collections import Iterable
+from diffpy.pdfmorph import morphs
+from diffpy.pdfmorph import refine
+from diffpy.pdfmorph import tools
+
+default_config = dict(scale=None, stretch=None, smear=None)
+
+# map of operation dict
+# TODO: include morphing on size
+morph_step_dict = dict(scale=morphs.MorphScale,
+                       stretch=morphs.MorphStretch,
+                       smear=[morphs.MorphXtalPDFtoRDF,
+                              morphs.MorphSmear,
+                              morphs.MorphXtalRDFtoPDF])
+
+def morph_engine(xobj, yobj, xref, yref, rmin=None, rmax=None,
+                 morph_config=default_config):
     """function to perfom PDF morphing.
 
     Parameters
     ----------
     morph_config : dict
-        valid keys:
 
         'rmin', 'rmax', 'rstep', 'scale', 'stretch',
-        'smear', 'baselineslope', 
+        'smear', 'baselineslope'
 
     Returns
     -------
@@ -18,41 +33,40 @@ def morph_engine(xobj, yobj, xref, yref, morph_config):
         normal rw value
     pcc : float
         pearson correlation coefficient
-
     """
-    from diffpy.pdfmorph import morphs
-    from diffpy.pdfmorph import refine
-    from diffpy.pdfmorph import tools
-
-    # define morph chian with config
-    refpars = []
+    operation_dict = {}
+    # configure default morph_dict
     if not rmin:
         rmin = min(xobj.min(), xref.min())
+        morph_config['rmin'] = rmin
     if not rmax:
         rmax = max(xobj.max(), xref.max())
+        morph_config['rmax'] = rmax
+    morph_config['rstep'] = None
+    if morph_config.get('smear') and not morph_config.get('baselineslope'):
+        morph_config['baselineslope'] = -0.5
 
     # config dict defines initial guess of parameters
-    if not morph_config.get('baselineslope', None):
-        morph_config["baselineslope"]=-0.5
     chain = morphs.MorphChain(config=morph_config)
 
-    # grid
+    # rgrid
     chain.append(morphs.MorphRGrid())
 
-    # scale
-    chain.append(morphs.MorphScale())
-    refpars.append("scale")
+    # morph operations
+    refpars = []
+    active_morphs = [k for k, v in morph_config.items() if v and k in
+                     morph_step_dict]
+    for k in active_morphs:
+        morph_cls = morph_step_dict[k]
+        if isinstance(morph_cls, Iterable):
+            for el in morph_cls:
+                chain.append(el())
+        else:
+            chain.append(morph_cls())
+        refpars.append(k)
 
-    # stretch
-    chain.append(morphs.MorphStretch())
-    refpars.append("stretch")
-
-    # smear
-    chain.append(morphs.MorphXtalPDFtoRDF())
-    chain.append(morphs.MorphSmear())
-    chain.append(morphs.MorphXtalRDFtoPDF())
-    refpars.append("smear")
-    refpars.append("baselineslope")
+    print("INFO: Following morphing steps are active:\n{}"
+          .format('\n'.join(refpars)))
 
     # define refiner
     refiner = refine.Refiner(chain, xobj, yobj, xref, yref)
