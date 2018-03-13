0fda11be6dfe991296c2e2931520e129100b2f09
Timothy Liu <chbrahmsliu@gmail.com>
2018-03-13 13:36:19 -0400

    WIP: revisit pdfmorph function. test utils in stretch, scale and smear

--- diffpy/pdfmorph/pdfmorph_func.py
+++ diffpy/pdfmorph/pdfmorph_func.py
@@ -1,15 +1,14 @@
 from collections import Iterable
 from diffpy.pdfmorph import morphs
-from diffpy.pdfmorph import refine
+from diffpy.pdfmorph import refine as ref
 from diffpy.pdfmorph import tools
 
 import matplotlib.pyplot as plt
 
-default_config = dict(scale=None, stretch=None, smear=None, qdamp=None,
-                      refine=True)
+default_config = dict(scale=None, stretch=None, smear=None, qdamp=None)
 
 # map of operation dict
-# TODO: include morphing on size
+# TODO: include morphing on psize
 morph_step_dict = dict(scale=morphs.MorphScale,
                        stretch=morphs.MorphStretch,
                        smear=[morphs.MorphXtalPDFtoRDF,
@@ -17,9 +16,9 @@ morph_step_dict = dict(scale=morphs.MorphScale,
                               morphs.MorphXtalRDFtoPDF],
                        qdamp=morphs.MorphResolutionDamping)
 
-def morph_engine(xobj, yobj, xref, yref, rmin=None, rmax=None,
-                 morph_config=default_config, pearson=False,
-                 addpearson=False):
+def pdfmorph(xobj, yobj, xref, yref, morph_config=morph_config,
+             rmin=None, rmax=None, pearson=False, addpearson=False,
+             fixed_operations=None, refine=True):
     """function to perfom PDF morphing.
 
     Parameters
@@ -28,12 +27,16 @@ def morph_engine(xobj, yobj, xref, yref, rmin=None, rmax=None,
 
         'rmin', 'rmax', 'rstep', 'scale', 'stretch',
         'smear', 'baselineslope'
+    fixed_operations : list
+        list of string specifying operations won't be refined.
 
     Returns
     -------
     chain : diffpy.pdfmorph.morphs.morphchain.MorphChain
         instance of morph chain. ``xobj, yobj, xref, yref = chain.xyallout``
         will return morphed results
+    rv_cfg : dict
+        dictionary of refined parameters
     rw : float
         normal rw value
     pcc : float
@@ -41,27 +44,27 @@ def morph_engine(xobj, yobj, xref, yref, rmin=None, rmax=None,
     """
     operation_dict = {}
     refpars = []
-    _cfg = morph_config
-    # configure default morph_dict
+    rv_cfg = dict(morph_config)
+    # configure r-range
     if not rmin:
         rmin = min(xobj.min(), xref.min())
-        _cfg['rmin'] = rmin
+        rv_cfg['rmin'] = rmin
     if not rmax:
         rmax = max(xobj.max(), xref.max())
-        _cfg['rmax'] = rmax
-    _cfg['rstep'] = None
+        rv_cfg['rmax'] = rmax
+    rv_cfg['rstep'] = None
+    # configure smear
+    if rv_cfg.get('smear') and not rv_cfg.get('baselineslope'):
+        rv_cfg['baselineslope'] = -0.5
 
     # config dict defines initial guess of parameters
-    chain = morphs.MorphChain(_cfg)
-
-    if _cfg.get('smear') and not _cfg.get('baselineslope'):
-        _cfg['baselineslope'] = -0.5
+    chain = morphs.MorphChain(rv_cfg)
 
     # rgrid
     chain.append(morphs.MorphRGrid())
 
     # morph operations
-    active_morphs = [k for k, v in _cfg.items() if v and k in
+    active_morphs = [k for k, v in rv_cfg.items() if (v is not None) and k in
                      morph_step_dict]
     for k in active_morphs:
         morph_cls = morph_step_dict[k]
@@ -72,33 +75,31 @@ def morph_engine(xobj, yobj, xref, yref, rmin=None, rmax=None,
             chain.append(morph_cls())
         refpars.append(k)
 
-    print("INFO: Following morphing steps are active:\n{}"
-          .format('\n'.join(refpars)))
+    # exclude fixed options
+    if fixed_operations:
+        for opt in fixed_operations:
+            refpars.remove(opt)
+        print("== INFO: Following morphing steps are fixed:\n{}\n =="
+              .format('\n'.join(fixed_operations)))
 
     # define refiner
-    refiner = refine.Refiner(chain, xobj, yobj, xref, yref)
+    refiner = ref.Refiner(chain, xobj, yobj, xref, yref)
     if pearson:
         refiner.residual = refiner._pearson
     if addpearson:
        refiner.residual = refiner._addpearson
     # execute morphing
-    if _cfg.get('refine') and refpars:
-        try:
-            # This works better when we adjust scale and smear first.
-            if "smear" in refpars:
-                rptemp = ["smear"]
-                if "scale" in refpars:
-                    rptemp.append("scale")
-                refiner.refine(*rptemp)
-            refiner.refine(*refpars)
-        except ValueError as e:
-            parser.error(str(e))
-    elif "smear" in refpars and _cfg['baselineslope'] is None:
-        try:
-            refiner.refine("baselineslope", baselineslope = -0.5)
-        except ValueError as e:
-            parser.error(str(e))
+    if refpars and refine:
+        # This works better when we adjust scale and smear first.
+        if "smear" in refpars:
+            rptemp = ["smear"]
+            if "scale" in refpars:
+                rptemp.append("scale")
+            refiner.refine(*rptemp)
+        # Refine all params
+        refiner.refine(*refpars)
     else:
+        # no operation if refine=False or refpars is empty list
         chain(xobj, yobj, xref, yref)
 
     # summary
@@ -108,16 +109,13 @@ def morph_engine(xobj, yobj, xref, yref, rmin=None, rmax=None,
     chain[0] = morphs.Morph()
     chain(xobj, yobj, xref, yref)
 
-    items = list(_cfg.items())
-    items.sort()
-    #output = "\n".join("# %s = %f"%i for i in items)
-    #output += "\n# Rw = %f" % rw
-    #output += "\n# Pearson = %f" % pcc
-    #print(output)
-    print("Rw = {}, Pearson corref = {}".format(rw, pcc))
-    print(_cfg, refpars)
+    output = "\n".join(["# {} = {:.3f}".format(k, rv_cfg[k])\
+            for k in active_morphs])
+    output += "\n# Rw = {:.3f}".format(rw)
+    output += "\n# Pearson = {:.3f}".format(pcc)
+    print(output)
 
-    return chain, rw, pcc
+    return chain, rv_cfg, rw, pcc
 
 
 def plot_morph(chain, fig=None, **kwargs):
