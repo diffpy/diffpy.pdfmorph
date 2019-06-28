2b572344403208b08d737f7796e42a33fb710b82
Timothy Liu <chbrahmsliu@gmail.com>
2019-06-28 13:15:39 -0400

    CLN/DOC: make default config and morph step dict private
    Update docstring accordingly

--- diffpy/pdfmorph/__init__.py
+++ diffpy/pdfmorph/__init__.py
@@ -22,5 +22,6 @@ __id__ = "$Id$"
 __version__ = '0.0.1'
 
 # top-level import
-from diffpy.pdfmorph.pdfmorph_api import pdfmorph, default_config, plot_morph
+from diffpy.pdfmorph.pdfmorph_api import (pdfmorph, morph_default_config, 
+                                          plot_morph)
 # End of file
