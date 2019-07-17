732afbb3931338b74247a2b21825212ba08768d6
Matthew Roman <m.roman@columbia.edu>
2019-07-17 15:48:35 -0400

    Changes to conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -10,18 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-import time
-import importlib
+# import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'PDFmorph'
-copyright = '2019, Chris Farrow, Pavol Juhas, Timothy Liu, Matthew Roman, Simon Billinge'
-author = 'Chris Farrow, Pavol Juhas, Timothy Liu, Matthew Roman, Simon Billinge'
+copyright = '2019, Chris Farrow, Pavol Juhás, Chia-Hao (Timothy) Liu, S. Matthew Roman, Simon J.L. Billinge'
+author = 'Chris Farrow, Pavol Juhás, Chia-Hao (Timothy) Liu, S. Matthew Román, Simon J.L. Billinge'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
@@ -32,30 +29,89 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+import sphinx_rtd_theme
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 
+	'sphinx.ext.todo', 'sphinx.ext.viewcode']
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+from jinja2 import Template, Environment, FileSystemLoader
+import os
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['Thumbs.db', '.DS_Store'] 
 
+pygments_style = 'sphinx'
+
+todo_include_todos = True
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_theme_options = {
-    'navigation_with_keys' : 'true',
-    'relbarbgcolor': 'black'
-}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+htmlhelp_basename = 'PDFmorphdoc'
+
+latex_documents = [
+	(master_doc, 'pdfmorph.tex', 'PDFmorph Documentation', 
+	'author', 'manual'),
+]
+
+man_pages = [
+	(master_doc, 'pdfmorph', 'PDFmorph Documentation', [author], 1)
+]
+
+texinfo_documents = [
+	(master_doc, 'PDFmorph', 'PDFmorph Documentation', author,
+	'PDFmorph', 'One line description of project.', 'Miscellaneous'),
+]
+
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
+
+epub_exclude_files = ['search.html']
+
+print('rendering templates')
+templated_files = os.listdir(os.path.join(os.path.dirname(__file__),
+				'_templates'))
+ctx = {'collection_env': 'collection-2019-0.1-pdfmorph',
+	'analysis_env': 'analysis-2019-0.1-pdfmorph',
+	'bs_proxy_config': '5577 5578',
+	'analysis_session_cmd': 'setup_analysis',
+	'start_analysis': 'start_analysis()',
+	'start_analysis_func': 'start_analysis',
+	'setup_analysis': 'setup_analysis',
+	'len': len,
+	'list': list,
+	}
+env = Environment(loader=FileSystemLoader([
+	'_templates',
+	os.path.join(os.path.dirname(__file__), '_templates'),
+	]))
+for tname in templated_files:
+	template = env.get_template(tname)
+	result = template.render(ctx)
+	with open(os.path.join(os.path.dirname(__file__), os.path.splitext(tname)[0] + '.rst'), 'wt') as f:
+		f.write(result)
