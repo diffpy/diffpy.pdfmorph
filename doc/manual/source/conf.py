f2f9e7cb6e4c1e66ce9cb6ceb8f3f7a789fdc762
Caden Myers <cjm2304@columbia.edu>
2024-05-21 13:41:46 -0400

    change sys.path to pathlib.Path in conf.py

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -7,13 +7,13 @@
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
+# add these directories to pathlib.Path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../../src'))
+pathlib.Path.insert(0, os.path.abspath('../..'))
+pathlib.Path.insert(0, os.path.abspath('../../../src'))
 
 
 # -- Project information -----------------------------------------------------
