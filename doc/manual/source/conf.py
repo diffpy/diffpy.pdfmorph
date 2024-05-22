e937e9a6c26c75ebcc3a1e63b00e915f82ffa2c2
Caden Myers <cjm2304@columbia.edu>
2024-05-22 10:58:52 -0400

    replace os with pathlib

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -11,9 +11,10 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import sys
-import os
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../../src'))
+from pathlib import Path
+
+sys.path.insert(0, str(Path('../../..').resolve()))
+sys.path.insert(0, str(Path('../../../src').resolve()))
 
 
 # -- Project information -----------------------------------------------------
