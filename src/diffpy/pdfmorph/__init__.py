816cd3d3d15debc63003ab8e4a0b2c235d28ee74
Caden Myers <cjm2304@columbia.edu>
2024-05-28 17:57:31 -0400

    comment out unused imports

--- src/diffpy/pdfmorph/__init__.py
+++ src/diffpy/pdfmorph/__init__.py
@@ -17,10 +17,10 @@
 """
 
 # top-level import
-from diffpy.pdfmorph.pdfmorph_api import morph_default_config, pdfmorph, plot_morph
+# from diffpy.pdfmorph.pdfmorph_api import morph_default_config, pdfmorph, plot_morph
 
 # obtain version information
-from diffpy.pdfmorph.version import __version__
+# from diffpy.pdfmorph.version import __version__
 
 # key used when saving multiple morphs
 __save_morph_as__ = "save_morph_as"
