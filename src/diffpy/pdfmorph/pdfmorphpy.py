2bdfe5ac01c77aa1574b18985012777981fab3eb
Sparks29032 <Sparks29032@gmail.com>
2024-11-01 00:34:15 +0900

    Add python interfacing

---
+++ src/diffpy/pdfmorph/pdfmorphpy.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+from diffpy.pdfmorph.pdfmorphapp import create_option_parser, single_morph, multiple_targets, multiple_morphs
+
+
+def pdfmorph(file1, file2, **kwargs):
+    """Run PDFmorph at Python level.
+
+    Parameters
+    ----------
+    file1: str
+        Path-like object to the file to be morphed.
+    file2: str
+        Path-like object to the target file.
+    kwargs: dict
+        See the PDFmorph manual for options.
+
+    Returns
+    -------
+    dict:
+        Summary of morphs.
+    """
+
+    parser = create_option_parser()
+
+    inputs = []
+    for key, value in kwargs.items():
+        inputs.append(f"--{key}")
+        inputs.append(f"{value}")
+    (opts, pargs) = parser.parse_args(inputs)
+    pargs = [file1, file2]
+
+    return single_morph(parser, opts, pargs)
+
+
+def pdfmorph_multiple_targets(file, dir, **kwargs):
+    """Run PDFmorph at Python level.
+
+        Parameters
+        ----------
+        file1: str
+            Path-like object to the file to be morphed.
+        file2: str
+            Path-like object to the target file.
+        kwargs: dict
+            See the PDFmorph manual for options.
+
+        Returns
+        -------
+        dict:
+            Summary of morphs.
+        """
+
+    parser = create_option_parser()
+
+    inputs = []
+    for key, value in kwargs.items():
+        inputs.append(f"--{key}")
+        inputs.append(f"{value}")
+    (opts, pargs) = parser.parse_args(inputs)
+    pargs = [file, dir]
+
+    return multiple_targets(parser, opts, pargs)
+
+
+def pdfmorph_multiple_morphs(dir, file, **kwargs):
+    """Run PDFmorph at Python level.
+
+        Parameters
+        ----------
+        file1: str
+            Path-like object to the file to be morphed.
+        file2: str
+            Path-like object to the target file.
+        kwargs: dict
+            See the PDFmorph manual for options.
+
+        Returns
+        -------
+        dict:
+            Summary of morphs.
+        """
+
+    parser = create_option_parser()
+
+    inputs = []
+    for key, value in kwargs.items():
+        inputs.append(f"--{key}")
+        inputs.append(f"{value}")
+    (opts, pargs) = parser.parse_args(inputs)
+    pargs = [dir, file]
+
+    return multiple_morphs(parser, opts, pargs)
